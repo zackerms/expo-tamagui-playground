8c4384e12d3e4c855a3e479acc2e9829d4cbec1b
zacker <dev.zacker@gmail.com>
2025-12-07 13:19:09 +0900

    feat: config のバージョンを更新

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,5 +1,5 @@
 import { createTamagui } from "@tamagui/core"
-import { config } from "@tamagui/config/v2"
+import { config } from "@tamagui/config/v3"
 
 export const tamaguiConfig = createTamagui(config)
 export type AppConfig = typeof tamaguiConfig
@@ -7,5 +7,3 @@ export type AppConfig = typeof tamaguiConfig
 declare module "tamagui" {
     interface TamaguiCustomConfig extends AppConfig {}
 }
-
-export default tamaguiConfig
