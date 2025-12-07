225c3397df379ae71dd177d000d6b008bfddaa4d
zacker <dev.zacker@gmail.com>
2025-12-07 13:18:59 +0900

    feat: 定義順を修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
 import "@tamagui/core/reset.css"
 
 import { AppProps } from "next/app"
-import React from "react"
+import React, { useMemo } from "react"
 import { AppToastProvider } from "components/AppToastProvider"
 import { TamaguiProvider } from "@tamagui/core"
 import { tamaguiConfig } from "tamagui.config"
 
 export default function App({ Component, pageProps }: AppProps) {
+    const contents = useMemo(() => {
+        return <Component {...pageProps} />
+    }, []);
+
     return (
-        <AppToastProvider>
-            <TamaguiProvider config={tamaguiConfig} disableInjectCSS>
-                <Component {...pageProps} />
-            </TamaguiProvider>
-        </AppToastProvider>
+        <TamaguiProvider config={tamaguiConfig} disableInjectCSS>
+            <AppToastProvider>
+                {contents}
+            </AppToastProvider>
+        </TamaguiProvider>
     )
 }
